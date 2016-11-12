336318902939bc87cc8413e148bfa4780a300255
stephen <wqstu1@gmail.com>
2016-11-12 20:45:04 +0800

    Update

--- leetcode/239. Sliding Window Maximum.cc
+++ leetcode/239. Sliding Window Maximum.cc
@@ -1,4 +1,24 @@
 // Copyright 2016 Qi Wang
+// Refer to: http://blog.csdn.net/xudli/article/details/46955257
+// Time complexity: O(n)
+// Date: 2016-11-12
+class Solution {
+ public:
+  vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    if (k == 0) return vector<int>();
+    vector<int> res;
+    list<size_t> window;
+    for (size_t i = 0; i < nums.size(); ++i) {
+      while (!window.empty() && nums[window.back()] <= nums[i])
+        window.pop_back();
+      window.push_back(i);
+      if (i - window.front() >= k) window.pop_front();
+      if (i + 1 >= k) res.push_back(nums[window.front()]);
+    }
+    return res;
+  }
+};
+
 // Date: 2016-11-11
 struct cmp {
   bool operator ()(const pair<int, size_t>& lhs, const pair<int, size_t>& rhs) {
--- leetcode/354. Russian Doll Envelopes.cc
+++ leetcode/354. Russian Doll Envelopes.cc
@@ -1,4 +1,67 @@
 // Copyright 2016 Qi Wang
+// Method 3
+// Binary search with std::lower_bound, turning problem into LIS
+// Refer to: http://www.cnblogs.com/grandyang/p/5568818.html
+// Date: 2016-11-12
+class Solution {
+ public:
+  int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+    if (envelopes.empty()) return 0;
+    sort(envelopes.begin(), envelopes.end(),
+        [](const pair<int, int>& lhs, const pair<int, int>& rhs) {
+          return lhs.first != rhs.first
+              ? lhs.first < rhs.first
+              : lhs.second > rhs.second;
+        });
+    vector<int> dp {envelopes[0].second};
+    for (size_t i = 1; i < envelopes.size(); ++i) {
+      auto it = lower_bound(dp.begin(), dp.end(), envelopes[i].second);
+      if (it == dp.end()) {
+        dp.push_back(envelopes[i].second);
+      } else {
+        *it = envelopes[i].second;
+      }
+    }
+    return static_cast<int>(dp.size());
+  }
+};
+
+// Method 2
+// Binary search, turning problem into LIS
+// Refer to: http://www.cnblogs.com/grandyang/p/5568818.html
+// Date: 2016-11-12
+class Solution {
+ public:
+  int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+    if (envelopes.empty()) return 0;
+    sort(envelopes.begin(), envelopes.end(),
+        [](const pair<int, int>& lhs, const pair<int, int>& rhs) {
+          return lhs.first != rhs.first
+              ? lhs.first < rhs.first
+              : lhs.second > rhs.second;
+        });
+    vector<int> dp {envelopes[0].second};
+    for (size_t i = 1; i < envelopes.size(); ++i) {
+      int l = 0, r = dp.size();
+      while (l < r) {
+        int mid = (l + r) >> 1;
+        if (dp[mid] < envelopes[i].second) {
+          l = mid + 1;
+        } else {
+          r = mid;
+        }
+      }
+      if (r == dp.size()) {
+        dp.push_back(envelopes[i].second);
+      } else {
+        dp[r] = envelopes[i].second;
+      }
+    }
+    return static_cast<int>(dp.size());
+  }
+};
+
+// Method 2
 // Date: 2016-11-11
 class Solution {
  public:
