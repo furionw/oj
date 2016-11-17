7335e8ad518fa7161c4e93f40047258f2e192c68
stephen <wqstu1@gmail.com>
2016-11-17 16:04:35 +0800

    Update

--- leetcode/263. Ugly Number.cpp
+++ leetcode/263. Ugly Number.cpp
@@ -1,17 +1,31 @@
+// Copyright 2016 Qi Wang
+// Date: 2016-11-17
+class Solution {
+ public:
+  bool isUgly(int num) {
+    if (num <= 0) return false;
+    for (; num % 2 == 0; num >>= 1) {}
+    for (; num % 3 == 0; num /= 3) {}
+    for (; num % 5 == 0; num /= 5) {}
+    return num == 1;
+  }
+};
+
+// Date: 2015-09
 class Solution 
 {
 public:
-    bool isUgly(int num) 
+  bool isUgly(int num) 
+  {
+    if (num <= 0)
     {
-    	if (num <= 0)
-    	{
-    		return false;
-    	}
-
-    	for (; num % 2 == 0; num /= 2) {}
-    	for (; num % 3 == 0; num /= 3) {}
-    	for (; num % 5 == 0; num /= 5) {}
-    		
-    	return num == 1;
+      return false;
     }
+
+    for (; num % 2 == 0; num /= 2) {}
+    for (; num % 3 == 0; num /= 3) {}
+    for (; num % 5 == 0; num /= 5) {}
+      
+    return num == 1;
+  }
 };
--- leetcode/264. Ugly Number II.cpp
+++ leetcode/264. Ugly Number II.cpp
@@ -1,21 +1,43 @@
+// Copyright 2016 Qi Wang
+// Date: 2016-11-17
+class Solution {
+ public:
+  int nthUglyNumber(int n) {
+    if (n <= 0) return 0;
+    int res[n];
+    res[0] = 1;
+    int multiplicators[] = {2, 3, 5};
+    int idxs[] = {0, 0, 0};
+    for (int i = 1; i < n; ++i) {
+      res[i] = INT_MAX;
+      for (int j = 0; j < 3; ++j)
+        res[i] = min(res[i], multiplicators[j] * res[idxs[j]]);
+      for (int j = 0; j < 3; ++j)
+        for (; multiplicators[j] * res[idxs[j]] <= res[i]; ++idxs[j]) {}
+    }
+    return res[n - 1];
+  }
+};
+
+// Date: 2015-09
 class Solution 
 {
 public:
-    int nthUglyNumber(int n) 
+  int nthUglyNumber(int n) 
+  {
+    vector<int> nums(n, 1);
+    int idx2 = 0; 
+    int idx3 = 0;
+    int idx5 = 0;
+    
+    for (int i = 1; i < n; ++ i)
     {
-        vector<int> nums(n, 1);
-        int idx2 = 0; 
-        int idx3 = 0;
-        int idx5 = 0;
-        
-        for (int i = 1; i < n; ++ i)
-        {
-        	nums[i] = min(2*nums[idx2], min(3*nums[idx3], 5*nums[idx5]));        	
-        	for (; 2 * nums[idx2] <= nums[i]; ++ idx2) {}
-        	for (; 3 * nums[idx3] <= nums[i]; ++ idx3) {}
-        	for (; 5 * nums[idx5] <= nums[i]; ++ idx5) {}
-        }
-        
-    	return nums[n-1];
+      nums[i] = min(2*nums[idx2], min(3*nums[idx3], 5*nums[idx5]));
+      for (; 2 * nums[idx2] <= nums[i]; ++ idx2) {}
+      for (; 3 * nums[idx3] <= nums[i]; ++ idx3) {}
+      for (; 5 * nums[idx5] <= nums[i]; ++ idx5) {}
     }
+      
+    return nums[n-1];
+  }
 };
