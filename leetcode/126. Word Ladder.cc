1cc046d363529a0816d9e7f043f64fe0143e4f21
stephen <wqstu1@gmail.com>
2016-11-20 17:07:34 +0800

    Update

--- leetcode/126. Word Ladder.cc
+++ leetcode/126. Word Ladder.cc
@@ -1,8 +1,36 @@
-#include <map>
-#include <queue>
+// Copyright 2016 Qi Wang
+// BFS
+// Date: 2016-11-20
+class Solution {
+ public:
+  int ladderLength(string beginWord, string endWord,
+      unordered_set<string>& wordList) {
+    // boost up the trivial case
+    if (beginWord == endWord) return 2;
+    queue<pair<string, int>> q;
+    q.emplace(beginWord, 2);
+    while (!q.empty()) {
+      string word = q.front().first;
+      int dis = q.front().second;
+      q.pop();
+      for (size_t i = 0; i < word.size(); ++i) {
+        for (char c = 'a'; c <= 'z'; ++c)
+          if (c != word[i]) {
+            swap(word[i], c);
+            if (word == endWord) return dis;
+            if (wordList.find(word) != wordList.end()) {
+              q.emplace(word, dis + 1);
+              wordList.erase(word);
+            }
+            swap(word[i], c);
+          }
+      }
+    }
+    return 0;
+  }
+};
 
-using namespace std;
-
+// Date: 2015-02
 class Solution 
 {
 public:
