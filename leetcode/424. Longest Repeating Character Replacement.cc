f5187a72b000e3e22890706e0974c291d655acae
stephen <wqstu1@gmail.com>
2016-12-02 08:36:32 +0800

    Update by adding some comments.

--- leetcode/424. Longest Repeating Character Replacement.cc
+++ leetcode/424. Longest Repeating Character Replacement.cc
@@ -4,10 +4,14 @@
 class Solution {
  public:
   int characterReplacement(string s, int k) {
+    // most + k is the size of the sliding window
     int cnt[26] = {0}, most = 0;
     for (int begin = 0, end = 0; end < s.size(); ++end) {
       // Try to widen the window size by updating "most"
       most = max(most, ++cnt[s[end] - OFFSET]);
+      // If the sliding window's size wasn't widen by the above operation
+      // and [begin, end]'s size, begin + end + 1, is larger than most + k,
+      // we simply shrink the left bound of the sliding window
       if (most + k == end - begin) {
         --cnt[s[begin] - OFFSET];
         ++begin;
