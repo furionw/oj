d9d1a4ff016d160c53cecccc2e758cf35a5eda39
stephen <wqstu1@gmail.com>
2016-12-25 22:52:42 +0800

    Update

--- leetcode/447. Number of Boomerangs.cc
+++ leetcode/447. Number of Boomerangs.cc
@@ -1,4 +1,24 @@
 // Copyright 2016 Qi Wang
+// Date: 2016-12-25
+class Solution {
+ public:
+  int numberOfBoomerangs(vector<pair<int, int>>& points) {
+    int res = 0;
+    for (size_t i = 0; i < points.size(); ++i) {
+      unordered_map<int, int> cnts;
+      for (size_t j = 0; j < points.size(); ++j) {
+        if (i == j) continue;
+        int xDelta = points[j].first - points[i].first,
+            yDelta = points[j].second - points[i].second;
+        ++cnts[xDelta * xDelta + yDelta * yDelta];
+      }
+      for (auto& p : cnts)
+        res += p.second * (p.second - 1);
+    }
+    return res;
+  }
+};
+
 // Date: 2016-11-08
 class Solution {
  public:
