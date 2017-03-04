53c22064a9e4f0de018cc399d34aeccbf079f1a9
stephen <wqstu1@gmail.com>
2017-03-04 22:57:09 +0800

    Update

--- leetcode/278. First Bad Version.cc
+++ leetcode/278. First Bad Version.cc
@@ -1,9 +1,25 @@
-// Copyright 2016 Qi Wang
-// Date: 2016-11-26
-
+// Copyright 2017 Qi Wang
 // Forward declaration of isBadVersion API.
 bool isBadVersion(int version);
 
+// Date: 2017-03-04
+class Solution {
+ public:
+  int firstBadVersion(int n) {
+    for (size_t l = 1, r = n; l <= r; ) {
+      int mid = (l + r) >> 1;
+      if (isBadVersion(mid)) {
+        if (mid - 1 == 0 || !isBadVersion(mid - 1)) return mid;
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return -1;  // comfort the compiler
+  }
+};
+
+// Date: 2016-11-26
 class Solution {
  public:
   int firstBadVersion(int n) {
