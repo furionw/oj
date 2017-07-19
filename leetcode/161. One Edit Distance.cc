4599a06e40ce07933fb36408ba18c57c4dc77efa
stephen <wqstu1@gmail.com>
2017-07-18 23:06:23 -0700

    Add

--- leetcode/161. One Edit Distance.cc
+++ leetcode/161. One Edit Distance.cc
@@ -1,9 +1,31 @@
 // Copyright 2017 Qi Wang
 // Date: 2017-07-18
+// Method 2
+// Refer to one of the leetcode solution.
 class Solution {
  public:
   bool isOneEditDistance(string s, string t) {
-    if (abs(int(s.size()) - int(t.size())) > 1) return false;
+    if (s.size() < t.size()) s.swap(t);
+    if (s.size() - t.size() > 1) return 0;
+    for (int i = 0; i < t.size(); i++) {
+      // The reference use "!s.compare(_1, _2, _3, _4, _5)" in the tenary op,
+      // but I think I can use s.substr() for the better readability.
+      if (s[i] != t[i])
+        return s.size() == t.size()
+            ? s.substr(i + 1) == t.substr(i + 1)
+            : s.substr(i + 1) == t.substr(i);
+    }
+    return s.size() == t.size() + 1;
+  }
+};
+
+// Date: 2017-07-18
+// Method 1
+class Solution {
+ public:
+  bool isOneEditDistance(string s, string t) {
+    if (s.size() > t.size()) swap(s, t);
+    if (t.size() - s.size() > 1) return false;
     int diff_cnt = 0;
     if (s.size() == t.size()) {
       for (size_t i = 0; i < s.size(); ++i) {
@@ -11,7 +33,6 @@ class Solution {
       }
       return 1 == diff_cnt;
     } else {
-      if (s.size() > t.size()) swap(s, t);
       for (size_t i = 0; i < s.size();) {
         if (s[i] != t[i + diff_cnt]) {
           if (diff_cnt) {
