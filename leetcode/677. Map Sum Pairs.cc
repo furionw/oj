d1b434ca072f7cdf4e9956549f34651fb9283ce4
stephen <wqstu1@gmail.com>
2017-10-16 16:53:45 +0800

    Update

--- leetcode/677. Map Sum Pairs.cc
+++ leetcode/677. Map Sum Pairs.cc
@@ -1,4 +1,25 @@
 // Copyright 2017 Qi Wang
+// Date: 2017-10-16
+class MapSum {
+ public:
+  void insert(string key, int val) {
+    str_to_int_map_[key] = val;
+  }
+  
+  int sum(string prefix) {
+    auto itr = str_to_int_map_.lower_bound(prefix);
+    int sum = 0;
+    for (; itr != str_to_int_map_.end() &&
+           itr->first.find(prefix) != string::npos; ++itr) {
+      sum += itr->second;
+    }
+    return sum;
+  }
+ 
+ private:
+  map<string, int> str_to_int_map_;
+};
+
 // Date: 2017-09-16
 // Case 1:
 //   insert("cloud", 4)
