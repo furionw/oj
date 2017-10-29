24f9053ded5b91ba839e4d934b831adb03e4e73a
stephen <wqstu1@gmail.com>
2017-10-29 18:40:42 +0800

    Update

--- leetcode/492. Construct the Rectangle.cc
+++ leetcode/492. Construct the Rectangle.cc
@@ -1,4 +1,17 @@
 // Copyright 2017 Qi Wang
+// Date: 2017-10-26
+class Solution {
+ public:
+  vector<int> constructRectangle(int area) {
+    for (int w = sqrt(area); w >= 1; --w) {
+      if (area % w == 0) {
+        return {area / w, w};
+      }
+    }
+    return {0, 0};
+  }
+};
+
 // Date: 2017-02-06
 class Solution {
  public:
