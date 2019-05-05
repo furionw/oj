cc2c8c1a1f3dd895acdcb197b5d1ebfe548b38ca
Stephen <wqstu1@gmail.com>
2019-05-06 00:07:56 +0100

    Add

---
+++ leetcode/1037. Valid Boomerang.py
@@ -0,0 +1,5 @@
+# Copyright 2019 Qi Wang
+# Date: 2019-05-06
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        return (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]) != (points[2][1] - points[0][1]) * (points[1][0] - points[0][0])
