ad587cfa00b9761058290c7cea5a130bad9f803a
Stephen <wqstu1@gmail.com>
2020-09-17 20:51:17 -0700

    Update

--- leetcode/1192. Critical Connections in a Network.java
+++ leetcode/1192. Critical Connections in a Network.java
@@ -1,4 +1,74 @@
-// Copyright 2019 Qi Wang
+// Date: 2020-09-15
+class Solution {
+  int ts = -1;
+  
+  public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+    List<Integer>[] g = new ArrayList[n];  // ?
+    for (int i = 0; i < n; ++i) {
+      g[i] = new ArrayList<Integer>();
+    }
+    for (List<Integer> connection : connections) {
+      int u = connection.get(0), v = connection.get(1);
+      g[u].add(v);
+      g[v].add(u);
+    }
+    boolean[] visited = new boolean[n];
+    boolean[] inStk = new boolean[n];
+    int[] enter = new int[n];
+    int[] low = new int[n];
+    Stack<Integer> stk = new Stack<Integer>();
+    for (int i = 0; i < n; ++i) {
+      if (!visited[i]) {
+        dfs(i, -1 /*parent*/, g, visited, enter, low, stk, inStk);
+      }
+    }
+    return connections.stream()
+        .filter(conn -> low[conn.get(0)] != low[conn.get(1)])
+        .collect(Collectors.toList());
+    // List<List<Integer>> result = new ArrayList<List<Integer>>();
+    // for (List<Integer> connection : connections) {
+    //   if (low[connection.get(0)] != low[connection.get(1)]) {
+    //     result.add(connection);
+    //   }
+    // }
+    // return result;
+  }
+  
+  private void dfs(
+      int u,
+      int parent,
+      List<Integer>[] g,
+      boolean[] visited,
+      int[] enter,
+      int[] low,
+      Stack<Integer> stk,
+      boolean[] inStk
+  ) {
+    visited[u] = true;
+    enter[u] = low[u] = ++ts;
+    stk.add(u);
+    inStk[u] = true;
+    g[u].stream()
+        .filter(v -> v != parent)
+        .forEach(v -> {
+          if (!visited[v]) {
+            dfs(v, u /*parent*/, g, visited, enter, low, stk, inStk);
+          }
+          if (inStk[v]) {
+            low[u] = Math.min(low[u], low[v]);
+          }
+        });
+    if (enter[u] == low[u]) {
+      for (int v = stk.pop(); v != u; v = stk.pop()) {
+        inStk[v] = false;
+        low[v] = enter[u];
+      }
+      inStk[u] = false;
+    }
+  }
+}
+
+
 // Date: 2019-10-27
 // Refer to: https://www.cnblogs.com/nullzx/p/7968110.html
 public class Solution {
