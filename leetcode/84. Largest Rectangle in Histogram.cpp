f0220b7d9ceb4dc9fbd41ce5b6b86ca9a6dbc682
Qi <wqstu1@gmail.com>
2022-11-04 19:14:45 -0700

    Update 84

--- leetcode/84. Largest Rectangle in Histogram.cpp
+++ leetcode/84. Largest Rectangle in Histogram.cpp
@@ -1,4 +1,30 @@
-// Copyright 2017 Qi Wang
+// Date: 2022-11-04
+// Refer to previous solution in 2014-09
+class Solution {
+ public:
+  int largestRectangleArea(vector<int>& height) {
+    height.push_back(-1);
+    stack<int> s;
+    int maxx = 0;
+    for (int i = 0; i < height.size();) {
+      if (s.empty() || height[s.top()] <= height[i]) {
+        s.push(i);
+        ++i;
+      } else {
+        int top = s.top();
+        s.pop();
+        maxx = max(
+            maxx,
+            height[top] * (
+                s.empty() ? i : (i - s.top() - 1)
+            )
+        );
+      }
+    }
+    return maxx;
+  }
+};
+
 // Date: 2014-09
 class Solution {
  public:
