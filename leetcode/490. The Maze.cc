6de61ffdecef2d0c23e5ee69e9e270f6344e796c
Qi Wang <wqstu1@gmail.com>
2025-07-24 22:23:05 -0700

    07-24

--- leetcode/490. The Maze.cc
+++ leetcode/490. The Maze.cc
@@ -1,4 +1,74 @@
-// Copyright 2017 Qi Wang
+// 2025-07-24
+class Solution {
+ public:
+  bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+    m = maze.size();
+    n = maze[0].size();
+    visited = vector<vector<bool>>(m, vector<bool>(n, false));
+    dx = destination[0];
+    dy = destination[1];
+    return f(maze, start[0], start[1]);
+  }
+
+ private:
+  bool f(vector<vector<int>>& maze, int x, int y) {
+    if (x == dx && y == dy) {
+      return true;
+    }
+
+    visited[x][y] = true;
+
+    // Note
+    // Refer to the Editorial, this can be implemented more easily by
+    // vector<int> dirX{0, 1, 0, -1};
+    // vector<int> dirY{-1, 0, 1, 0};
+
+    // up
+    {
+      int upX = x;
+      for (; upX - 1 >= 0 && maze[upX - 1][y] == 0; --upX) {}
+      if (!visited[upX][y] && f(maze, upX, y)) {
+        return true;
+      }
+    }
+
+    // down
+    {
+      int downX = x;
+      for (; downX + 1 < m && maze[downX + 1][y] == 0; ++downX) {}
+      if (!visited[downX][y] && f(maze, downX, y)) {
+        return true;
+      }
+    }
+
+    // left
+    {
+      int leftY = y;
+      for (; leftY - 1 >= 0 && maze[x][leftY - 1] == 0; --leftY) {}
+      if (!visited[x][leftY] && f(maze, x, leftY)) {
+        return true;
+      }
+    }
+
+    // right
+    {
+      int rightY = y;
+      for (; rightY + 1 < n && maze[x][rightY + 1] == 0; ++rightY) {}
+      if (!visited[x][rightY] && f(maze, x, rightY)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  int m;
+  int n;
+  int dx;
+  int dy;
+  vector<vector<bool>> visited;
+};
+
 // Date: 2017-02-06
 // Refer to: https://discuss.leetcode.com/topic/77590/simple-c-dfs-solution-using-set-to-mark-the-visited-end-points
 
